748de05b57095840ba24e3fde4dbc69811993fe6
Gláucio Oliveira <glaucio@goliveira.com>
2020-01-23 16:05:57 -0300

    updating get page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace webhook.Controllers
         [HttpGet]
         public string Get()
         {
-            return $"WebHook Server On - {DateTime.Now}";
+            return $"WebHook Server On - {DateTime.Now}\n(c) Copyright G. Oliveira - {DateTime.Now.Year}";
         }
 
         [HttpPost]
